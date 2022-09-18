6a733a3dddd809171c4d6ef90ce5a8d64e0bb06f
d-horowitz <d0556776910@outlook.co.il>
2022-09-18 16:27:16 +0300

    stage 0 0 0

---
+++ Stage0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static partial void Welcome6591();
+        static partial void Welcome6591()
+        {
+            Console.WriteLine("Welcome6591");
+        }
+    }
+}
