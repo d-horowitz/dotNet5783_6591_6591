f71809a489d6ed52101c44958abd13a18d82cc7f
d-horowitz <d0556776910@outlook.co.il>
2022-09-18 15:54:42 +0300

    stage 0

---
+++ Stage0/Program6591.cs
@@ -0,0 +1,13 @@
+﻿
+
+///<summary>
+///ygyuhuihui
+/// </summary>
+static void Welcome6591(int rr)
+{
+    Console.Write("{0} Enter your name: ", rr);
+    string name = Console.ReadLine();
+    Console.WriteLine("{0}, welcome to my first application", name);
+}
+
+Welcome6591(3);
