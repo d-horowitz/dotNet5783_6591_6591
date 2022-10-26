d71eb292a7b2691511b868214540dfc0545d1700
d-horowitz <d0556776910@outlook.co.il>
2022-10-26 13:25:20 +0300

    stage 1 13 started config

--- Stage1/DalList/DataSource.cs
+++ Stage1/DalList/DataSource.cs
@@ -4,9 +4,16 @@ namespace Dal;
 static internal class DataSource
 {
     static readonly Random randomer = new Random();
-    static internal Product[] Products = new Product[50];
-    static internal Order[] Orders = new Order[100];
-    static internal OrderItem[] OrderItems = new OrderItem[200];
+    static internal Product[] products = new Product[50];
+    static internal Order[] orders = new Order[100];
+    static internal OrderItem[] orderItems = new OrderItem[200];
 
+    static internal class Config
+    {
+        static internal int productIndex = 0;
+        static internal int orderIndex = 0;
+        static internal int orderItemIndex = 0;
+        static internal int productId = 100000;
+    }
 }
 
