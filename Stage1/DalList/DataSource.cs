8c3cde1d28f2e9f9c49537e70c0ac1219f5d524e
d-horowitz <d0556776910@outlook.co.il>
2022-10-30 15:30:46 +0200

    stage 1 15 finished (?) config

--- Stage1/DalList/DataSource.cs
+++ Stage1/DalList/DataSource.cs
@@ -3,18 +3,32 @@ namespace Dal;
 
 static internal class DataSource
 {
-    static readonly Random randomer = new Random();
-    static internal Product[] products = new Product[50];
-    static internal Order[] orders = new Order[100];
-    static internal OrderItem[] orderItems = new OrderItem[200];
+    static internal readonly Random _randomer = new Random();
+    static internal Product[] _products = new Product[50];
+    static internal Order[] _orders = new Order[100];
+    static internal OrderItem[] _orderItems = new OrderItem[200];
 
     static internal class Config
     {
-        static internal int productIndex = 0;
-        static internal int orderIndex = 0;
-        static internal int orderItemIndex = 0;
-        static internal int OrderId = 100000;
-        static internal int OrderItemId = 100000;
+        static internal int _productIndex = 0;
+        static internal int _orderIndex = 0;
+        static internal int _orderItemIndex = 0;
+        static internal int _orderId = 100000;
+        static internal int _orderItemId = 100000;
+        public static int orderId { get { return _orderId++; } }
+        public static int orderItemId { get { return _orderItemId++; } }
     }
-}
+    private static void addProducts()
+    {
+        
+    }
+    private static void addOrders()
+    {
+
+    }
+    private static void addOrderItems()
+    {
+
+    }
+};
 
