ab26c5ad0e4d1ee19aa0657f34548caac58c81bc
d-horowitz <102904217+d-horowitz@users.noreply.github.com>
2023-02-20 15:51:14 +0200

    stage 7 1

--- Stage1/PL/MainWindow.xaml.cs
+++ Stage1/PL/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using BlApi;
 using PL.Orders;
 
@@ -63,6 +51,11 @@ namespace PL
             catch(Exception ex){
                 MessageBox.Show("Please enter a valid order number.", "⚠"+ex.Message);
             }
+        }
+
+        private void StartSimulation(object sender, RoutedEventArgs e)
+        {
+            new Simulation.Simulation().Show();
         }
     }
 }
---
+++ Stage1/PL/Simulation/Simulation.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows;
+
+namespace PL.Simulation
+{
+    /// <summary>
+    /// Interaction logic for Simulation.xaml
+    /// </summary>
+    public partial class Simulation : Window, INotifyPropertyChanged
+    {
+        private bool timing = true;
+        private Thread timer;
+        private DateTime start = DateTime.Now;
+        private BackgroundWorker bw;
+        public Simulation()
+        {
+            InitializeComponent();
+            Loaded += ToolWindow_Loaded;
+            //TimeDisplay.DataContext = new { t = DateTime.Now - start };
+            TimeDisplay.DataContext = new { t = DateTime.Now.ToLongTimeString() };
+            bw = new();
+            timer = new(runTimer);
+            timer.Start();
+        }
+        private void runTimer()
+        {
+            while (timing)
+            {
+                UpdateTime(this, new RoutedEventArgs());
+                Thread.Sleep(1000);
+            }
+        }
+
+        //==================Hide X Button==================>>
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        void ToolWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Code to remove close box from window
+            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+        }
+        //==================Hide X Button==================^^
+
+        private void StopSimulation(object sender, RoutedEventArgs e)
+        {
+            timing = false;
+            Close();
+        }
+        private void UpdateTime(object sender, RoutedEventArgs e)
+        {
+            if (!CheckAccess())
+            {
+                Dispatcher.BeginInvoke(UpdateTime, sender, e);
+            }
+            else
+            {
+                //TimeDisplay.DataContext = new { t = (DateTime.Now - start).ToString().Substring(0, 8) };
+                TimeDisplay.DataContext = new { t = DateTime.Now.ToLongTimeString() };
+            }
+        }
+
+    }
+}
